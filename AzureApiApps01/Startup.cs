dce1b7c6b5bebdcbbf04988658d2dc7f4b7d6342
balamurugan <you@example.com>
2021-10-26 01:36:56 +0530

    [feat] added docker support

--- AzureApiApps01/Startup.cs
+++ AzureApiApps01/Startup.cs
@@ -42,9 +42,10 @@ namespace AzureApiApps01
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureApiApps01 v1"));
+               
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AzureApiApps01 v1"));
 
             app.UseHttpsRedirection();
 
